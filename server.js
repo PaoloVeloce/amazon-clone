33e674ec391b0e63ea9f7661184496cc6fcf53e4
Paolo Veloce <pablo.veloce@gmail.com>
2016-07-13 00:31:28 +0300

    add signup ejs page with 3 inputs and labels. Install express-flash, express-session and cookir parser and put errors checking inside signup ejs

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var engine = require('ejs-mate');
+// deep into logging system with session and cookie-parser
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var flash = require('express-flash');
 
 
 var User = require('./models/user');
 
 var app = express();
-
+// connecting mongo database to our server
 mongoose.connect('mongodb://root:1QaZ2WsX3EdC@ds017185.mlab.com:17185/amazonclone', function(err) {
    if (err) {
      console.log(err);
@@ -18,39 +22,34 @@ mongoose.connect('mongodb://root:1QaZ2WsX3EdC@ds017185.mlab.com:17185/amazonclon
    }
 });
 
-
+// ???
 app.use(express.static(__dirname + '/public'));
 // middleware - will log all user requests to the server
 app.use(morgan('dev'));
 // middleware - express app could parse json data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(cookieParser());
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: "Paolo!!!"
+}));
+app.use(flash());
+
+
+
 // middleware ejs - learn node how to use ejs
 app.engine('ejs', engine); // what kind of engine to use
 app.set('view engine', 'ejs'); // use ejs engine
 
+// introducing moved to routes folder routes
+var mainRoutes = require('./routes/main');
+var userRoutes = require('./routes/user');
+// middleware - say node that routes here - can be like '/paolo', mainRoutes
+app.use(mainRoutes);
+app.use(userRoutes);
 
-// app.post() - authentication
-app.post('/create-user', function(req, res, next) {
-  var user = new User();
-
-  user.profile.nome = req.body.name;
-  user.password = req.body.password;
-  user.email = req.body.email;
-
-  user.save(function(err) {
-    if (err) return next(err);
-    res.json('Succesfully created user')
-  });
-});
-
-app.get('/', function(req, res) {
-  res.render('home');
-});
-
-app.get('/about', function(req, res) {
-  res.render('main/about');
-})
 
 // running simpliest server
 app.listen(3000, function (err) {
